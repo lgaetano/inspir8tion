9e0d2505f5e2b1041c81e74d9283a6f9d3410ddd
Karina <infernormal@gmail.com>
2022-01-04 13:16:29 -0800

    new_card_form_code

---
+++ src/components/NewCardForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './NewCardForm.css';
+
+const NewCardForm = ({addCardCallback}) => {
+
+  const [formFields,setFormFields] = useState ({
+    message: ''
+  })
+  
+  const onMessageChange = (event) => {
+    setFormFields({
+        ...formFields,
+        message: event.target.value
+    })
+  };
+  
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  
+          addCardCallback({
+              message: formFields.message
+          });
+  
+          setFormFields({
+              message: ''
+          });
+  
+  };
+  
+    return (
+      <div>
+        <form onSubmit={onFormSubmit}>
+            <div>
+                <label htmlFor="message">Message:</label>
+                <input name="messsage" value={formFields.message} onChange={onMessageChange} />
+            </div>
+            <input
+                type="submit"
+                value="Add Card" />
+        </form>
+      </div>
+    );
+      
+    
+  };
+  NewCardForm.propTypes = {
+    addCardCallback: PropTypes.func.isRequired
+  };
+  
+  
+  export default NewCardForm;
