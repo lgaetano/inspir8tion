2f68458087448288b64ce568a491a1bbd569413c
Victoria Duke <toriduke@alumni.stanford.edu>
2022-01-04 12:11:45 -0800

    creates selectedBoard state

--- src/App.js
+++ src/App.js
@@ -1,62 +1,76 @@
-import './App.css';
-import NewBoardForm from './components/NewBoardForm';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Board from './components/Board';
-import BoardList from './components/BoardList';
+import "./App.css";
+import NewBoardForm from "./components/NewBoardForm";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Board from "./components/Board";
+import BoardList from "./components/BoardList";
 
-export const URL = 'https://inspir8tion-board.herokuapp.com/boards';
+export const URL = "https://inspir8tion-board.herokuapp.com/boards";
 
 const App = () => {
-    const [boards, setBoards] = useState([]);
-    const [status, setStatus] = useState('Loading...');
-    const [isBoardFormVisible, setBoardForm] = useState(true);
-  
-    const updateBoardFormVisibility = () => {
-      if (isBoardFormVisible === true)
-        setBoardForm(false)
+  const [boards, setBoards] = useState([]);
+  const [status, setStatus] = useState("Loading...");
+  const [isBoardFormVisible, setBoardForm] = useState(true);
+  const [selectedBoard, setSelectedBoard] = useState(
+    "Select a Board from the Board List!"
+  );
 
-      else {setBoardForm(true)}
+  const updateBoardFormVisibility = () => {
+    if (isBoardFormVisible === true) setBoardForm(false);
+    else {
+      setBoardForm(true);
     }
+  };
 
-    useEffect(() => {
-      axios
-        .get(URL)
-        .then((res) => {
-          const newBoards = res.data.map((board) => {
-            return {
-              id: board.board_id,
-              title: board.title,
-              owner: board.owner,
-            };
-          });
-          setStatus('Loaded');
-          setBoards(newBoards);
-        })
-        .catch((err) => {
-          console.log(err);
+  const chooseBoard = (title, owner) => {
+    console.log(`${title} + "-" + ${owner}`);
+  };
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        const newBoards = res.data.map((board) => {
+          return {
+            id: board.board_id,
+            title: board.title,
+            owner: board.owner,
+          };
         });
-    }, []);
+        setStatus("Loaded");
+        setBoards(newBoards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <NewBoardForm isBoardFormVisible={isBoardFormVisible} updateBoardFormVisibility={updateBoardFormVisibility}/>
-        <Board />
+        <NewBoardForm
+          isBoardFormVisible={isBoardFormVisible}
+          updateBoardFormVisibility={updateBoardFormVisibility}
+        />
       </header>
       <main>
-      <div>
-          {status === 'Loading...' ? (
+        <div>
+          {status === "Loading..." ? (
             `${status}`
           ) : (
-            <BoardList
-              boards={boards}
-            />
+            <BoardList boards={boards} />
           )}
+        </div>
+        <div>
+          <Board
+            onBoardSelect={selectedBoard}
+            boards={boards}
+            chooseBoard={chooseBoard}
+          />
         </div>
       </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,22 +1,23 @@
-import { useState } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
 
 const Board = (props) => {
-    // console.log(title);
-    return (
-        <>
-            <li>
-                Title column: {props.title}
-            </li>
-        </>
-    );
+  // console.log(title);
+  return (
+    <div>
+      <li onClick={props.chooseBoard}>Title column: {props.title}</li>
+
+      <h2>{props.onBoardSelect}</h2>
+    </div>
+  );
 };
 
 Board.propTypes = {
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    owner: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  owner: PropTypes.string.isRequired,
+  onBoardSelect: PropTypes.func,
 };
 
 export default Board;
