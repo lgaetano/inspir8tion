c768f46fa7e22fe133bba8aae7d94634080da3fa
Victoria Duke <toriduke@alumni.stanford.edu>
2022-01-06 18:55:29 -0800

    creates NewCardForm CSS

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,54 +1,49 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './NewCardForm.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./NewCardForm.css";
 
 const NewCardForm = ({ addCardCallback }) => {
+  const [formFields, setFormFields] = useState({
+    message: "",
+  });
 
-  const [formFields,setFormFields] = useState ({
-    message: ''
-  })
-  
   const onMessageChange = (event) => {
     setFormFields({
-        ...formFields,
-        message: event.target.value
-    })
+      ...formFields,
+      message: event.target.value,
+    });
   };
-  
+
   const onFormSubmit = (event) => {
     event.preventDefault();
-    
-          addCardCallback({
-              message: formFields.message
-          });
-  
-          setFormFields({
-              message: ''
-          });
-  
-  };
-  
-    return (
-      <form onSubmit={onFormSubmit}>
-          <div>
-              <label htmlFor="message">Message:</label>
-              <input 
-                name="message" 
-                value={formFields.message} 
-                onChange={onMessageChange} 
-              />
-          </div>
-          <input
-              type="submit"
-              value="Add Card" 
-          />
-      </form>
-    );
+
+    addCardCallback({
+      message: formFields.message,
+    });
+
+    setFormFields({
+      message: "",
+    });
   };
 
-  NewCardForm.propTypes = {
-    addCardCallback: PropTypes.func.isRequired
-  };
-  
+  return (
+    <form onSubmit={onFormSubmit}>
+      <div>
+        {/* <label htmlFor="message">Message:</label> */}
+        <input
+          placeholder="TYPE IN CARD"
+          name="message"
+          value={formFields.message}
+          onChange={onMessageChange}
+        />
+      </div>
+      <input type="submit" value=" +  add card" />
+    </form>
+  );
+};
 
-  export default NewCardForm;
+NewCardForm.propTypes = {
+  addCardCallback: PropTypes.func.isRequired,
+};
+
+export default NewCardForm;
